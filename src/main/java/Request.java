c2527ec490cb347cb5edc962b425c3b1ad8cecd7
qwchua <chuaqiwang@gmail.com>
2022-08-29 06:24:44 +0800

    add Request class

---
+++ src/main/java/Request.java
@@ -0,0 +1,11 @@
+public class Request {
+    private String name;
+
+    public Request(String name) {
+        this.name = name;
+    }
+
+    public void printRequestName() {
+        System.out.println(name);
+    }
+}
