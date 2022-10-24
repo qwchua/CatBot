18613ddbf316a59f8a0fec5636b34ee909d87a82
qwchua <chuaqiwang@gmail.com>
2022-10-25 04:30:04 +0800

    add gradle

---
+++ src/test/java/catbot/CatBotTest.java
@@ -0,0 +1,12 @@
+package catbot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CatBotTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2,2);
+    }
+}
